63c396abdac010a4ddfc9fbc597af65f4c4e3293
Aigiz Iskuzhin <tur3is@yandex.ru>
2026-04-17 10:14:53 +0500

    Prepare app for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const base = process.env.GITHUB_ACTIONS === 'true' && repositoryName
+  ? `/${repositoryName}/`
+  : '/';
+
 export default defineConfig({
+  base,
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
